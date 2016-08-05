fbe089975a2695dd36ba25d4d9324497939ba4a8
anon <anon@example.com>
2016-08-05 11:47:13 +0200

    Setup own postgresql config

--- Backend/mobilesandbox/settings.py
+++ Backend/mobilesandbox/settings.py
@@ -18,7 +18,8 @@ DATABASES = {
         'NAME': 'ms_db',                    # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': 'ms_user',
-        #'PASSWORD': '2HmUKLvf',
+        'HOST':'localhost',
+        'PASSWORD': '2HmUKLvf',
         #'HOST': '127.0.0.1',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         #'PORT': '5327',                      # Set to empty string for default.
     }
