db3867b17714e0156ae55a92a744e0d41fc393e7
anon <anon@example.com>
2016-08-05 12:33:58 +0200

    Got postgresql running only with HOST

--- Backend/mobilesandbox/settings.py
+++ Backend/mobilesandbox/settings.py
@@ -19,7 +19,7 @@ DATABASES = {
         # The following settings are not used with sqlite3:
         'USER': 'ms_user',
         'HOST':'localhost',
-        'PASSWORD': '2HmUKLvf',
+        #'PASSWORD': None,
         #'HOST': '127.0.0.1',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         #'PORT': '5327',                      # Set to empty string for default.
     }
