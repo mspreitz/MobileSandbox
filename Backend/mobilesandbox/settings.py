42e721e0499ca6f7fdd00235eb734e00e8c61d4f
mkauschi <m.kauschinger@bruzzzla.de>
2016-12-09 12:11:09 +0100

    Add some more styles

--- Backend/mobilesandbox/settings.py
+++ Backend/mobilesandbox/settings.py
@@ -23,10 +23,8 @@ DATABASES = {
         'NAME': 'ms_db',                    # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': 'ms_user',
-        #'HOST':'localhost',
         'HOST':'',
         'PASSWORD': '2HmUKLvf',
-        #'HOST': '127.0.0.1',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '5432'                      # Set to empty string for default
     }
 }
